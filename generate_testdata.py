f5435612a6754821b8807006de1043db5630ec50
Souta Ito <soutaito@info-lounge.jp>
2021-06-26 11:53:08 +0900

    Add todos

---
+++ generate_testdata.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+
+'''
+# plod:HighLevelCloseContact を任意の割合で含むschema:Eventインスタンスを指定の個数生成する
+plod:HighLevelCloseContact を満たすclassの条件
+下記の(1. or 2.) and 3. and 4.であること
+1. schema:locationが存在し、それの指すリソースの型がplod:DropletReachableActivityをplod:affordするプロパティが2つ以上持っている。
+2. plod:DropletReachableActivity型のschema:actionが2つ以上存在する
+3. plod:timeが存在し、15分以上である
+4. plod:RiskActivitySituation型のplod:situationOfActivityが1つ以上存在する
+(疑問点) schema:Eventに含まれるplod:agentの数はplod:HighLevelCloseContactの評価に使用していない？
+
+## 処理手順
+
+### 事前準備
+SARS-CoV-2_Infection_Risk_Ontology_cardinality.owlをパースして
+- schema:Placeを継承するクラスを再帰的に抽出してschema:locationの候補を取得
+    - それぞれがplod:DropletReachableActivityを2つ以上持つかを判定しておく
+- plod:Activityを継承するクラスを再帰的に抽出してschema:actionの候補を取得
+- plod:RiskActivitySituationを継承するクラスを再帰的に抽出してplod:situationOfActivityの候補を取得
+    - それぞれがplod:RiskActivitySituationを把握
+
+### テストデータ生成
+- schema:locationの候補となるクラスのダミーインスタンスを1つずつ生成
+```
+  <plod:Dummy rdf:about="http://plod.info/rdf/id/{xxx}}">
+    <rdf:type rdf:resource="http://plod.info/rdf/{xxx}"/>
+  </plod:Dummy>
+```
+- schema:Eventを作成し、rdf:about="http://plod.info/rdf/id/event_{xxx}"で一意となるURIを生成
+- schema:locationプロパティから上述のダミーインスタンスを参照する。plod:DropletReachableActivityを2つ以上持つかの判定を内部保持
+- plod:timeのダミーインスタンスを
+```
+  <time:Interval rdf:about="http://plod.info/rdf/id/time_{xxx}">
+    <time:hasBeginning rdf:datatype="http://www.w3.org/2001/XMLSchema#dateTime">{ISO 8601}</time:hasBeginning>
+    <time:hasDuration>
+      <time:TemporalDuration rdf:about="http://plod.info/rdf/id/duration_a42">
+        <rdfs:label>duration_a42</rdfs:label>
+        <time:numericDuration rdf:datatype="http://www.w3.org/2001/XMLSchema#decimal">{decimal}</time:numericDuration>
+      </time:TemporalDuration>
+    </time:hasDuration>
+
+    <time:hasEnd rdf:datatype="http://www.w3.org/2001/XMLSchema#dateTime">{ISO 8601}</time:hasEnd>
+  </time:Interval>
+```
+の形式で生成し、schema:Eventのplod:timeプロパティで参照。15分以上(900秒以上）かどうかの判定を内部保持
+
+- plod:agentを`<plod:agent rdf:resource="http://plod.info/rdf/id/person_{xxx}"/>`の形式でテストプロパティを適当な数生成
+- plod:actionを任意の数生成。含まれるplod:DropletReachableActivity型の種類が2以上かどうかを判定して内部保持
+- plod:situationOfActivityを任意の数生成。含まれるplod:RiskActivitySituation型の種類が2以上かどうかを判定して内部保持
+- 生成が完了したら、そのschema:Eventがplod:HighLevelCloseContactかどうかを判定。例えば100個テストデータを生成した後、そのうちいくつがplod:HighLevelCloseContactになるはずかをコメント等で出力する
+'''
+
+
