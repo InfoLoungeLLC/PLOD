847524149a3e841bd476f7347601f16b34572ea4
Souta Ito <soutaito@info-lounge.jp>
2021-07-26 13:37:41 +0900

    Update reasoning pattern

--- test/reasoning.py
+++ test/reasoning.py
@@ -6,34 +6,27 @@ start_time = time.time()
 
 my_world = World(filename="./reasoning.sqlite")
 my_world = World()
-onto = my_world.get_ontology("../rdf/SARS-CoV-2_Infection_Risk_Ontology_cardinality.owl").load()
+onto = my_world.get_ontology(
+    "../rdf/SARS-CoV-2_Infection_Risk_Ontology_cardinality.owl").load()
 # data = my_world.get_ontology("../rdf/example/PLOD_r3.rdf").load()
 data = my_world.get_ontology("./test.rdf").load()
-sync_reasoner([onto,data])
+sync_reasoner([onto, data])
 my_world.save()
 
-results = list(my_world.sparql("""
-    PREFIX plod: <http://plod.info/rdf/>
-    SELECT DISTINCT * WHERE {
-        ?s rdf:type plod:HighLevelCloseContact .
-    } limit 1000"""))
-ids = []
-for result in results:
-  ids.append(result[0].iri)  
-ids.sort()
-# print(ids)
-print("plod:HighLevelCloseContact count by reasoning.py: %s" % (len(ids)-1))
+types = ['HighLevelCloseContact', 'HighLevelClosedSpace', 'HighLevelCrowding',
+         'MediumLevelCloseContact', 'MediumLevelClosedSpace', 'MediumLevelCrowding']
 
-results = list(my_world.sparql("""
-    PREFIX plod: <http://plod.info/rdf/>
-    SELECT DISTINCT * WHERE {
-        ?s rdf:type plod:MediumLevelCloseContact .
-    } limit 1000"""))
-ids = []
-for result in results:
-  ids.append(result[0].iri)  
-ids.sort()
-# print(ids)
-print("plod:MediumLevelCloseContact count by reasoning.py: %s" % len(ids))
+for t in types:
+    results = list(my_world.sparql("""
+      PREFIX plod: <http://plod.info/rdf/>
+      SELECT DISTINCT * WHERE {
+          ?s rdf:type plod:%s .
+      } limit 1000""" % t))
+    ids = []
+    for result in results:
+        ids.append(result[0].iri)
+    # ids.sort()
+    # print(ids)
+    print("plod:%s count by reasoning.py: %s" % (t, len(ids)))
 
 print("--- %s seconds ---" % (time.time() - start_time))
