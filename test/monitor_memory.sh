75114397e2a189580c4e9e65cbe03154a09e1aec
Souta Ito <soutaito@info-lounge.jp>
2021-08-16 11:06:01 +0900

    Add memory usage % and re-fix index

--- test/monitor_memory.sh
+++ test/monitor_memory.sh
@@ -3,7 +3,7 @@ csv="memory_result/memory_case_"$1"_datacount_"$2".csv"
 rm $csv
 touch $csv
 echo "writing to memory.csv"
-echo "TIME_STAMP,Java Memory Usage (MB)" | tee -a $csv
+echo "TIME_STAMP,Java Memory Usage (%),Java Memory Usage (MB)" | tee -a $csv
 total="$(free -m | grep Mem | tr -s ' ' | cut -d ' ' -f 2)"
 echo $total
 sleep 1
@@ -11,11 +11,12 @@ sleep 1
 while true
 do
     DATE=`date +"%H:%M:%S:%s%:z"`
-    echo -n "$DATE, " | tee -a $csv
-    var="$(top -b -n 1| grep -w java | tr -s ' ' | cut -d ' ' -f 10)"
-    monitor=`top -b -n 1| grep -w java | tr -s ' ' | cut -d ' ' -f 10`
+    echo -n "$DATE," | tee -a $csv
+    var="$(top -b -n 1| grep -w java | tr -s ' ' | cut -d ' ' -f 11)"
+    monitor=`top -b -n 1| grep -w java | tr -s ' ' | cut -d ' ' -f 11`
     echo $monitor
     if [[ `echo "$monitor > 0" | bc` == 1 ]]; then
+        echo -n "$var," | tee -a $csv
         echo "scale=3; ($var*$total/100)" | bc | tee -a $csv
     else
         break
